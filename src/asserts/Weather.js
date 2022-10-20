8f698475ee700d53fbb2f220dcb9794cf8782555
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-10-20 20:06:23 +0530

    Update Weather.js

--- src/asserts/Weather.js
+++ src/asserts/Weather.js
@@ -5,11 +5,13 @@ import {useGlobalContext} from '../ContextApi/Context'
 const Weather = () =>{
     const {current} = useGlobalContext();
     if(current.weather_descriptions === 'Sunny'){
-        return (<i className="fas fa-sun my-4" style={{fontSize:200, color: "orangered"}}></i>)
+        return (<i className="fas fa-sun my-4" style={{fontSize:200, color: "orange"}}></i>)
     }else if(current.weather_descriptions === 'Clouds'){
         return (<i className="fas fa-cloud my-4" style={{fontSize:200, color: "#f1f2f6"}}></i>)
     }else if(current.weather_descriptions === 'Rainy'){
         return (<i className="fas fa-rain my-4" style={{fontSize:200, color: "#a4b0be"}}></i>)
+    }else if(current.weather_descriptions === 'Clear'){
+        return (<i className="fas fa-sun my-4" style={{fontSize:200, color: "orangered"}}></i>)
     }else{
         return (<i className="fas fa-cloud my-4" style={{fontSize:200, color: "#f1f2f6"}}></i>)
     }
